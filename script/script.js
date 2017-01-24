2d601a5d28fd306b652ff15e92d0b519a16b9008
d-ashokkumar <ashokkumar01cbe@gmail.com>
2017-01-24 11:21:08 +0530

    commit files for the first time

---
+++ script/script.js
@@ -0,0 +1,82 @@
+function initFunction() {
+ if (store.getAll().todo === undefined) {
+  store.set("todo", {
+   length: 0
+  });
+
+ } else {
+  console.log(store.getAll().todo);
+ }
+ $("#addTodoItem").on("click", function () {
+  addTextToList();
+  $("#todoItem").val("");
+ });
+
+ printValues();
+}
+
+function addTextToList() {
+ console.log(store.getAll().todo)
+ var value = $("#todoItem").val();
+ if (value === "") {
+  alert("Enter a text")
+ } else {
+  var obj = {
+   name: value,
+   finished: false
+  }
+  var length = store.getAll().todo.length + 1;
+  store.transact("todo", function (values) {
+   values["" + length] = obj;
+  });
+  store.transact("todo", function (values) {
+   values.length = length;
+  });
+  insertFinishedValue(value, false, length);
+ }
+}
+
+function printValues() {
+ var values = (store.getAll().todo);
+ var length = values.length;
+ console.log(values)
+ for (loop = 0; loop < length; loop++) {
+  insertFinishedValue(values[loop + 1].name, values[loop + 1].finished, loop + 1);
+ }
+ $("body").on("click", "input.inputCheckBox", function (event) {
+  changeValues(event);
+ });
+}
+
+function insertFinishedValue(value, finished, idValue) {
+ console.log(idValue)
+ var parentDiv = $("<div>").attr({
+  "class": "Unfinished col-md-12",
+  id: idValue,
+ });
+ $("#displayItems").append(parentDiv);
+ var checkBox = $("<input>").attr({
+  type: "checkBox",
+  class: "inputCheckBox"
+ }).css({
+  float: "left"
+ });
+ if (finished === true) {
+  $(checkBox).attr("checked", "checked");
+ }
+ var div = $("<div>").attr({
+  class: "value col-md-6"
+ });
+ $(div).text(value);
+ $(parentDiv).append(checkBox);
+ $(parentDiv).append(div);
+}
+
+function changeValues(event) {
+ var id = ($(event)[0].target.offsetParent.id);
+ var currentValue = store.getAll().todo[id].finished;
+ console.log(currentValue)
+ store.transact("todo", function (values) {
+  values[id].finished = !currentValue;
+ });
+}
